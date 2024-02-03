776ce56250bb23b477cad6748f44b66e8bcfd830
exrawon <152187690+exrawon@users.noreply.github.com>
2024-02-03 16:31:11 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,17 +3,20 @@ document.addEventListener('DOMContentLoaded', () => {
 	const lapras = document.createElement('div');
 	const banner = document.querySelector('.banner');
 	const waves = document.querySelector('.waves');
+	const water = document.querySelector('.water');
 
 	let isPlaying = false;
 	let isGameOver = true;
-	let platformCount = 8;
+	let platformCount = 6;
 	let platforms = [];
 	let score = 0;
 	let highScore = 0;
 	let laprasLeftSpace = 50;
-	let startPoint = 150;
+	let startPoint = 120;
 	let laprasBottomSpace = startPoint;
 	let isJumping = true;
+	let isGoingLeft = false;
+	let isGoingRight = false;
 	let upTimerId;
 	let downTimerId;
 	let leftTimerId;
@@ -80,18 +83,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	const playJump = () => {
 		let audio = new Audio('./assets/jump.mp3');
+		audio.preload = 'auto';
 		audio.play();
 	};
 	const playCry = () => {
 		let audio = new Audio('./assets/cry.mp3');
+		audio.preload = 'auto';
+		audio.play();
+	};
+
+	const playCountdown = () => {
+		let audio = new Audio('./assets/countdown.mp3');
+		audio.preload = 'auto';
 		audio.play();
 	};
 
 	class Platform {
 		constructor(newPlatBottom) {
-			this.left = Math.random() * 335;
+			this.left = Math.random() * 319;
 			this.bottom = newPlatBottom;
 			this.visual = document.createElement('div');
+			this.width = 100;
 
 			const visual = this.visual;
 			visual.classList.add('platform');
@@ -112,6 +124,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	function movePlatforms() {
 		if (laprasBottomSpace > 200) {
+			//default 150
 			platforms.forEach((platform) => {
 				if (score <= 20) {
 					platform.bottom -= 2;
@@ -121,7 +134,7 @@ document.addEventListener('DOMContentLoaded', () => {
 					platform.bottom -= 4;
 				} else if (score <= 200) {
 					platform.bottom -= 5;
-				} else platform.bottom -= 7;
+				} else platform.bottom -= 6;
 
 				let visual = platform.visual;
 				visual.style.bottom = platform.bottom + 'px';
@@ -141,7 +154,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	function createLapras() {
 		lapras.classList.add('lapras');
-		laprasLeftSpace = platforms[0].left - 20;
+		laprasLeftSpace = platforms[0].left - 5;
 		lapras.style.left = laprasLeftSpace + 'px';
 		lapras.style.bottom = laprasBottomSpace + 'px';
 		grid.appendChild(lapras);
@@ -151,6 +164,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		setTimeout(() => {
 			isJumping = false;
 		}, 100);
+
 		clearTimeout(upTimerId);
 		downTimerId = setInterval(() => {
 			laprasBottomSpace -= 3;
@@ -160,19 +174,20 @@ document.addEventListener('DOMContentLoaded', () => {
 			}
 			platforms.map((platform, index) => {
 				if (
-					laprasBottomSpace >= platform.bottom - 10 &&
-					laprasBottomSpace <= platform.bottom + 10 &&
-					laprasLeftSpace + 70 >= platform.left &&
-					laprasLeftSpace <= platform.left + 50 &&
+					laprasBottomSpace >= platform.bottom &&
+					laprasBottomSpace <= platform.bottom + 15 &&
+					laprasLeftSpace + 95 >= platform.left &&
+					laprasLeftSpace <= platform.left + platform.width - 10 &&
 					!isJumping &&
 					laprasBottomSpace < 500
 				) {
 					startPoint = laprasBottomSpace;
+					platform.width = 65;
 					jump();
 					playJump();
 					setTimeout(() => {
 						platform.visual.style.backgroundImage = 'url(./assets/ice.png)';
-						platform.visual.style.width = '60px';
+						platform.visual.style.width = platform.width + 'px';
 					}, 100);
 				}
 			});
@@ -182,10 +197,18 @@ document.addEventListener('DOMContentLoaded', () => {
 	function jump() {
 		clearInterval(downTimerId);
 		isJumping = true;
+		if (isGoingLeft) {
+			lapras.classList.add('jumping-left');
+		}
+		if (isGoingRight) {
+			lapras.classList.add('jumping-right');
+		}
 		upTimerId = setInterval(() => {
 			laprasBottomSpace += 3;
 			lapras.style.bottom = laprasBottomSpace + 'px';
 			if (laprasBottomSpace > startPoint + 210) {
+				lapras.classList.remove('jumping-left');
+				lapras.classList.remove('jumping-right');
 				fall();
 			}
 		}, 5);
@@ -194,6 +217,9 @@ document.addEventListener('DOMContentLoaded', () => {
 	function moveLeft() {
 		clearInterval(leftTimerId);
 		clearInterval(rightTimerId);
+		isGoingLeft = true;
+		isGoingRight = false;
+		lapras.classList.remove('jumping-right');
 
 		leftTimerId = setInterval(() => {
 			if (laprasLeftSpace >= -50) {
@@ -207,6 +233,9 @@ document.addEventListener('DOMContentLoaded', () => {
 	function moveRight() {
 		clearInterval(leftTimerId);
 		clearInterval(rightTimerId);
+		isGoingLeft = false;
+		isGoingRight = true;
+		lapras.classList.remove('jumping-left');
 
 		rightTimerId = setInterval(() => {
 			if (laprasLeftSpace <= 347) {
@@ -227,9 +256,9 @@ document.addEventListener('DOMContentLoaded', () => {
 		if (isGameOver && e.key === ' ') {
 			start();
 		}
-		if (e.key === 'a') {
+		if (isPlaying && e.key === 'a') {
 			moveLeft();
-		} else if (e.key === 'd') {
+		} else if (isPlaying && e.key === 'd') {
 			moveRight();
 		} else if (e.key === 's' || 'w') {
 			moveStraight();
@@ -237,10 +266,11 @@ document.addEventListener('DOMContentLoaded', () => {
 	}
 	function gameOver() {
 		surface.style.zIndex = -2;
+		isPlaying = false;
 		isGameOver = true;
 		banner.classList.add('clickable');
 		banner.classList.remove('disabled');
-		banner.innerText = `PLAY AGAIN`;
+		banner.innerText = `🎮\u00A0\u00A0\u00A0PLAY AGAIN\u00A0\u00A0\u00A0🎮`;
 		while (grid.firstChild) {
 			grid.removeChild(grid.firstChild);
 		}
@@ -262,34 +292,63 @@ document.addEventListener('DOMContentLoaded', () => {
 		clearInterval(movePlatformId);
 	}
 
+	function countdown() {
+		banner.classList.remove('clickable');
+		banner.classList.add('disabled');
+		banner.classList.add('countdown');
+		banner.innerText = `3`;
+		playCountdown();
+		setTimeout(() => {
+			banner.innerText = `2`;
+		}, 1000);
+		setTimeout(() => {
+			banner.innerText = `1`;
+		}, 2000);
+		setTimeout(() => {
+			banner.innerText = `JUMP!`;
+		}, 3000);
+		setTimeout(() => {
+			fall();
+			isPlaying = true;
+			banner.classList.remove('countdown');
+			banner.innerText = `${score}\u00A0 🌊\u00A0\u00A0\u00A0`;
+		}, 4000);
+	}
+
 	function start() {
 		surface.style.zIndex = 20;
 		isGameOver = false;
 		if (!isGameOver) {
 			laprasLeftSpace = 50;
-			startPoint = 150;
+			startPoint = 120;
 			laprasBottomSpace = startPoint;
 			score = 0;
-			banner.classList.remove('clickable');
-			banner.classList.add('disabled');
-			banner.innerText = `${score}\u00A0 🌊\u00A0\u00A0\u00A0`;
 			platforms = [];
 			grid.innerHTML = '';
 			createPlatforms();
 			createLapras();
+			countdown();
 			movePlatformId = setInterval(movePlatforms, 10);
-			fall();
 		}
 	}
-	grid.innerHTML =
-		'<div class="title"a>Lapras Jump</div><div class="version">ver. 1.3b</div><img class= "title-img" src="./assets/lapras.gif" alt="">';
-	document.addEventListener('keydown', control);
-	banner.addEventListener('click', start);
-	const titleImg = document.querySelector('.title-img');
-	titleImg.addEventListener('click', playCry);
-	waves.addEventListener('click', () => {
-		if (isGameOver) {
-			start();
-		}
-	});
+
+	function initialize() {
+		grid.innerHTML =
+			'<div class="title"a>Lapras Jump</div><div class="version">ver. 1.5</div><img class= "title-img" src="./assets/lapras.gif" alt="">';
+		const titleImg = document.querySelector('.title-img');
+		titleImg.addEventListener('click', playCry);
+		document.addEventListener('keydown', control);
+		banner.addEventListener('click', start);
+		waves.addEventListener('click', () => {
+			if (isGameOver) {
+				start();
+			}
+		});
+		water.addEventListener('click', () => {
+			if (isGameOver) {
+				start();
+			}
+		});
+	}
+	initialize();
 });
